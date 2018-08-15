919c68e484c1f7c11e41c2b90b98bffbc738f9d1
seiwen <593837810@qq.com>
2018-08-15 23:55:30 +0800

    test

---
+++ vue_test/my-project/src/views/todo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="todo">
+        <input v-model="val" @keydown.enter="add" type="text" />
+        <ul>
+            <li v-for="(a,index) in arr" :key="index">
+                {{ a }} <button @click="remove(index)">删除</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            add() {
+                this.arr.unshift(this.val),
+                this.val = ''
+            },
+            remove(i) {
+                this.arr = this.arr.filter((item,index) => index !== i)
+            }
+        },
+        data() {
+            return {
+                arr: [],
+                val: ''
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
