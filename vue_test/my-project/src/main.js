84a6bf554171244162db73b5a9e93641333c4343
王立峰 <wanglifeng@globalegrow.com>
2018-08-16 18:15:53 +0800

    '制作简单的vue购物车'

--- vue_test/my-project/src/main.js
+++ vue_test/my-project/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import $ from 'jquery'
 
 Vue.config.productionTip = false
-
+Vue.config.debug = true
 new Vue({
   store,
   router,
+  $,
   render: h => h(App)
 }).$mount('#app')
--- vue_test/my-project/src/views/todo.vue
+++ vue_test/my-project/src/views/todo.vue
@@ -6,12 +6,30 @@
                 {{ a }} <button @click="remove(index)">删除</button>
             </li>
         </ul>
+        <div class="btn-group" role="group" aria-label="...">
+            <button type="button" class="btn btn-default">Left</button>
+            <button type="button" class="btn btn-default">Middle</button>
+            <button type="button" class="btn btn-default">Right</button>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
+        //专门用来发送ajax的方法
+        created() {// 在数据被初始化后会调用，this指向指的也是VM实例，（钩子函数）
+            this.getData();
+        },
         methods: {
+            getData() {
+                //Promise 解决回调问题的
+                axios.get('./json/cart.json').then((result) => {
+                    this.products = result.data;
+                }).catch((err) => {
+                    console.log(err)
+                });
+            },
             add() {
                 this.arr.unshift(this.val),
                 this.val = ''
@@ -23,7 +41,8 @@
         data() {
             return {
                 arr: [],
-                val: ''
+                val: '',
+                products: []
             }
         },
     }
